d02ab313d73792166a4100c307ac97cf54de0d03
Monzurul Hasan <49614333+mhs003@users.noreply.github.com>
2022-02-16 12:28:20 +0600

    Update index.php

--- api/index.php
+++ api/index.php
@@ -1,5 +1,9 @@
 <?php
+echo "Hello World... \n<pre>";
 $reqUrl = parse_url($_SERVER['REQUEST_URL']);
+print_r($reqUrl);
+
+/*
 if(isset($reqUrl['path'])){
   $file = $reqUrl['path'];
 } else {
@@ -7,7 +11,6 @@ if(isset($reqUrl['path'])){
 }
 echo $file;
 
-/*
 if(!file_exists('..'.$file)){
   echo "<h1>404 not found</h1>";
   echo "<p>The file <b>{$file}</b> does not exists in this server</p>";
