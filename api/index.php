1d74584de3a1179b4f65e3b89df5d807d42ca641
Monzurul Hasan <49614333+mhs003@users.noreply.github.com>
2022-02-16 12:23:33 +0600

    Update index.php

--- api/index.php
+++ api/index.php
@@ -10,5 +10,6 @@ if(!file_exists(__DIR__.'/..'.$file)){
   echo "<h1>404 not found</h1>";
   echo "<p>The file <b>{$file}</b> does not exists in this server</p>";
   exit;
+} else {
+  require_once __DIR__.'/..'.$file;
 }
-require_once __DIR__.'/..'.$file;
