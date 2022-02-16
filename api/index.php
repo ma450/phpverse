30bd737954985bf568aae4f3a7e8b17546534079
Monzurul Hasan <49614333+mhs003@users.noreply.github.com>
2022-02-16 12:30:00 +0600

    Update index.php

--- api/index.php
+++ api/index.php
@@ -1,15 +1,12 @@
 <?php
-echo "Hello World... \n<pre>";
-$reqUrl = parse_url($_SERVER['REQUEST_URL']);
+$reqUrl = parse_url($_SERVER['REQUEST_URI']);
 print_r($reqUrl);
 
-/*
 if(isset($reqUrl['path'])){
   $file = $reqUrl['path'];
 } else {
   $file = '/index.php';
 }
-echo $file;
 
 if(!file_exists('..'.$file)){
   echo "<h1>404 not found</h1>";
@@ -18,4 +15,3 @@ if(!file_exists('..'.$file)){
 } else {
   require_once '..'.$file;
 }
-*/
