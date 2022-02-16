53c9802f541694198e77a2bb463aeb64557b4483
Monzurul Hasan <49614333+mhs003@users.noreply.github.com>
2022-02-16 12:44:57 +0600

    Update index.php

--- api/index.php
+++ api/index.php
@@ -4,12 +4,12 @@ $reqUrl = parse_url($_SERVER['REQUEST_URI']);
 if(isset($reqUrl['path'])){
   $file = $reqUrl['path'];
 } else {
-  $file = '/index.php';
+  $file = '/index';
 }
 
-if(!file_exists(__DIR__'/..'.$file)){
+if(!file_exists(__DIR__.'/..'.$file.'.php')){
   include '404.php';
   exit;
 } else {
-  require_once __DIR__'/..'.$file;
+  require_once __DIR__.'/..'.$file.'.php';
 }
