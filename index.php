fb31351ea43bc7824085d3dd1cefa515b2d775a5
ma450 <38773698+ma450@users.noreply.github.com>
2022-04-23 20:48:34 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,15 +1,13 @@
 <?php
 
 
-$__version__  = '3.3.1';
+$__version__  = '3.1.2';
 $__password__ = '123456';
 $__hostsdeny__ = array(); // $__hostsdeny__ = array('.youtube.com', '.youku.com');
 $__content_type__ = 'image/gif';
-//$__content_type__ = 'text/html';
 $__timeout__ = 20;
 $__content__ = '';
-$__chunked__= 0;
-$__trailer__= 0;
+
 
 function message_html($title, $banner, $detail) {
     $error = <<<MESSAGE_STRING
@@ -85,37 +83,25 @@ function decode_request($data) {
 
 
 function echo_content($content) {
-    global $__password__, $__content_type__,$__chunked__,$__content__;
-    $chunk="";
-    if($__chunked__==1) {
-    	if(empty($__content__)) {
-    		$chunk=sprintf("%x\r\n%s\r\n", strlen($content), $content);
-	} else {
-    	        $chunk=$content;
-	}
-    } else {
-    	        $chunk=$content;
-    }
-    //
+    global $__password__, $__content_type__;
     if ($__content_type__ == 'image/gif') {
-        $chunk = $chunk ^ str_repeat($__password__[0], strlen($chunk));
+        echo $content ^ str_repeat($__password__[0], strlen($content));
     } else {
-        $chunk = $chunk;
+        echo $content;
     }
-    echo $chunk;
 }
 
 
 function curl_header_function($ch, $header) {
-    global $__content__, $__content_type__,$__chunked__;
+    global $__content__, $__content_type__;
     $pos = strpos($header, ':');
     if ($pos == false) {
         $__content__ .= $header;
     } else {
         $key = join('-', array_map('ucfirst', explode('-', substr($header, 0, $pos))));
-        //if ($key != 'Transfer-Encoding') {
+        if ($key != 'Transfer-Encoding') {
             $__content__ .= $key . substr($header, $pos);
-        //}
+        }
     }
     if (preg_match('@^Content-Type: ?(audio/|image/|video/|application/octet-stream)@i', $header)) {
         $__content_type__ = 'image/x-png';
@@ -123,21 +109,17 @@ function curl_header_function($ch, $header) {
     if (!trim($header)) {
         header('Content-Type: ' . $__content_type__);
     }
-    if (preg_match('@^Transfer-Encoding: ?(chunked)@i', $header)) {
-        $__chunked__ = 1;
-    }
     return strlen($header);
 }
 
 
 function curl_write_function($ch, $content) {
-    global $__content__,$__chunked__,$__trailer__;
+    global $__content__;
     if ($__content__) {
         // for debug
         // echo_content("HTTP/1.0 200 OK\r\nContent-Type: text/plain\r\n\r\n");
         echo_content($__content__);
         $__content__ = '';
-	$__trailer__ = $__chunked__;
     }
     echo_content($content);
     return strlen($content);
@@ -145,14 +127,13 @@ function curl_write_function($ch, $content) {
 
 
 function post() {
-    global $__content_type__;
     list($method, $url, $headers, $kwargs, $body) = @decode_request(@file_get_contents('php://input'));
 
     $password = $GLOBALS['__password__'];
     if ($password) {
         if (!isset($kwargs['password']) || $password != $kwargs['password']) {
             header("HTTP/1.0 403 Forbidden");
-            echo message_html('403 Forbidden', 'Wrong Password', "please confirm your password.");
+            echo message_html('403 Forbidden', 'Wrong Password', "please edit proxy.ini");
             exit(-1);
         }
     }
@@ -163,7 +144,6 @@ function post() {
         $host = $urlparts['host'];
         foreach ($hostsdeny as $pattern) {
             if (substr($host, strlen($host)-strlen($pattern)) == $pattern) {
-                header('Content-Type: ' . $__content_type__);
                 echo_content("HTTP/1.0 403\r\n\r\n" . message_html('403 Forbidden', "hostsdeny matched($host)",  $url));
                 exit(-1);
             }
@@ -173,9 +153,9 @@ function post() {
     if ($body) {
         $headers['Content-Length'] = strval(strlen($body));
     }
-    //if (isset($headers['Connection'])) {
-    //    $headers['Connection'] = 'close';
-    //}
+    if (isset($headers['Connection'])) {
+        $headers['Connection'] = 'close';
+    }
 
     $header_array = array();
     foreach ($headers as $key => $value) {
@@ -198,13 +178,10 @@ function post() {
             break;
         case 'PUT':
         case 'DELETE':
-        case 'OPTIONS':
-        case 'PATCH':
             $curl_opt[CURLOPT_CUSTOMREQUEST] = $method;
             $curl_opt[CURLOPT_POSTFIELDS] = $body;
             break;
         default:
-            header('Content-Type: ' . $__content_type__);
             echo_content("HTTP/1.0 502\r\n\r\n" . message_html('502 Urlfetch Error', 'Invalid Method: ' . $method,  $url));
             exit(-1);
     }
@@ -230,32 +207,14 @@ function post() {
     curl_setopt_array($ch, $curl_opt);
     $ret = curl_exec($ch);
     $errno = curl_errno($ch);
-    if ($GLOBALS['__content__'] && $GLOBALS['__trailer__']==0 ) {
+    if ($GLOBALS['__content__']) {
         echo_content($GLOBALS['__content__']);
     } else if ($errno) {
-        $content = "HTTP/1.0 502\r\n\r\n" . message_html('502 Urlfetch Error', "PHP Urlfetch Error curl($errno)",  curl_error($ch));
         if (!headers_sent()) {
             header('Content-Type: ' . $__content_type__);
-            echo_content($content);
-        } else if($errno==CURLE_OPERATION_TIMEOUTED) {
-	    if($GLOBALS['__chunked__']==1) {
-            	$content = "-1\r\n\r\n";//fake chunked end flag
-	        $GLOBALS['__chunked__']=0;
-	        $GLOBALS['__trailer__']=0;
-	    } else {
-            	$content = "";
-	    }
-            echo_content($content);
         }
-    }
-    //when chunked there may be trailer
-    if ($GLOBALS['__trailer__']==1 && $GLOBALS['__content__']){
-	    $GLOBALS['__chunked__']=0;
-            echo_content("0\r\n".$GLOBALS['__content__']."\r\n");
-    }
-    //normal chunked end
-    if ($GLOBALS['__chunked__']==1){
-        echo_content("");
+        $content = "HTTP/1.0 502\r\n\r\n" . message_html('502 Urlfetch Error', "PHP Urlfetch Error curl($errno)",  curl_error($ch));
+        echo_content($content);
     }
     curl_close($ch);
 }
